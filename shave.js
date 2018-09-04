47303d6d70dc80e997bc0c76ba336b245a02f743
Jonah Caplan <jonahcaplan@gmail.com>
2018-09-04 00:16:12 -0400

    Fixup shave box bounds

--- shave.js
+++ shave.js
@@ -47,8 +47,8 @@ function start() {
 
 function paint(pointer, x, y) {
     if (pointer.isDown) {
-        for(var i = 1; i < 10; i++) {
-            for(var j = 1; j < 10; j++) {
+        for(var i = 0; i < 10; i++) {
+            for(var j = 0; j < 10; j++) {
                 mask.setPixel(x + i, y + j, 0, 0, 0, false);
             }
         }
